a20737b1dadb5b0e7bc83ff6d134d65dbd69e731
Yashwanth A Doddegowda <100475760+yashwanth-yasp@users.noreply.github.com>
2025-09-08 15:55:24 +0530

    removed all variables other than runner_token and hostname

--- terraform/setup.sh
+++ terraform/setup.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
 # === Install GitHub Runner ===
-ORG_NAME="find-your-bias"   # Change to your GitHub org
-RUNNER_NAME=$(hostname)
-RUNNER_DIR="/home/ubuntu/actions-runner"
-REG_TOKEN="${runner_token}"   # <-- Directly injected by Terraform
 
 # Install dependencies
 sudo apt-get update -y
 sudo apt-get install -y curl tar jq
 
 # Create runner dir
-sudo mkdir -p $RUNNER_DIR && cd $RUNNER_DIR
+sudo mkdir -p /home/ubuntu/actions-runner && cd /home/ubuntu/actions-runner
 
 curl -o actions-runner.tar.gz -L https://github.com/actions/runner/releases/download/v2.328.0/actions-runner-linux-x64-2.328.0.tar.gz
 tar xzf ./actions-runner.tar.gz
@@ -20,10 +16,9 @@ rm actions-runner.tar.gz
 
 # Configure runner
 ./config.sh --unattended \
-  --url https://github.com/${ORG_NAME} \
-  --token ${REG_TOKEN} \
-  --name ${RUNNER_NAME} \
-  --labels ${LABELS} \
+  --url https://github.com/find-your-bias \
+  --token ${runner_token} \
+  --name ${hostname} \
   --work _work
 
 # Install service
