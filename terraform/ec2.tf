1ebdf20b7fc8a65df7b285202000b374b193c65b
Yashwanth A Doddegowda <100475760+yashwanth-yasp@users.noreply.github.com>
2025-09-08 16:00:41 +0530

    modify terraform

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "demo" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  key_name      = "doddegowdanv.pem"
+  key_name      = "doddegowdanv"
 
   user_data = templatefile("${path.module}/setup.sh", {
     runner_token = var.runner_token
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t2.micro"
+  default     = "t2.medium"
 }
 
 variable "ami_id" {
