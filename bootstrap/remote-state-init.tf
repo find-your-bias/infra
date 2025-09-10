969b4b340e4ce9eb6d7c22509be167e4a5570d36
Yashwanth A Doddegowda <100475760+yashwanth-yasp@users.noreply.github.com>
2025-09-10 15:35:06 +0530

    add latest library changes

--- bootstrap/remote-state-init.tf
+++ bootstrap/remote-state-init.tf
@@ -1,16 +1,20 @@
 resource "aws_s3_bucket" "tf_state" {
   bucket = "find-your-bias-terraform-state"
-  acl    = "private"
+}
 
-  versioning {
-    enabled = true
+resource "aws_s3_bucket_versioning" "tf_state" {
+  bucket = aws_s3_bucket.tf_state.id
+  versioning_configuration {
+    status = "Enabled"
   }
+}
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
   }
 }
