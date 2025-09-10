74640e3e3008138bbe08e4f50d76bee74b35b8e7
Yashwanth A Doddegowda <100475760+yashwanth-yasp@users.noreply.github.com>
2025-09-10 15:26:24 +0530

    add bootstrap file

---
+++ terraform/bootstrap/remote-state-init.tf
@@ -0,0 +1,27 @@
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "find-your-bias-terraform-state"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = "find-your-bias-terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
