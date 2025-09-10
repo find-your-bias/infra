cf6f682329bf8f221aaac206555baff972345ff1
Yashwanth A Doddegowda <100475760+yashwanth-yasp@users.noreply.github.com>
2025-09-10 15:44:13 +0530

    modify main.tf to include backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,16 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "find-your-bias-terraform-state"
+    key            = "env/find-your-bias/terraform.tfstate" # choose a path structure per env/project
+    region         = "us-east-1"
+    dynamodb_table = "find-your-bias-terraform-locks"
+    encrypt        = true
+  }
 }
+
 
 provider "aws" {
   region = var.aws_region
