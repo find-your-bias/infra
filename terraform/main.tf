c4f857e5cfbe4b159f50146b6800bcdda8ef4c5e
Yashwanth A Doddegowda <100475760+yashwanth-yasp@users.noreply.github.com>
2025-09-12 10:07:19 +0530

    modified main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,13 +7,13 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket         = "find-your-bias-terraform-state"
-    key            = "env/find-your-bias/terraform.tfstate" # choose a path structure per env/project
-    region         = "us-east-1"
-    dynamodb_table = "find-your-bias-terraform-locks"
-    encrypt        = true
-  }
+  # backend "s3" {
+  #   bucket         = "find-your-bias-terraform-state"
+  #   key            = "env/find-your-bias/terraform.tfstate" # choose a path structure per env/project
+  #   region         = "us-east-1"
+  #   dynamodb_table = "find-your-bias-terraform-locks"
+  #   encrypt        = true
+  # }
 }
 
 
